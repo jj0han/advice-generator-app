8eb93bbc78b158cad7153f836c54a7ac8e5d790f
jj0han <joaovieirabomfim@gmail.com>
2023-03-06 23:26:23 -0400

    finished app

--- App.js
+++ App.js
@@ -1,24 +1,25 @@
 import { StatusBar } from 'expo-status-bar'
 import { useState } from 'react'
-import { Image, Text, View } from 'react-native'
+import { Image, SafeAreaView, Text, View } from 'react-native'
 import { TailwindProvider } from 'tailwindcss-react-native'
 import Button from './components/Button'
-import pattern from './images/pattern-divider-mobile.svg'
+import pattern from './images/pattern-divider-mobile.png'
+import * as Animatable from 'react-native-animatable'
 
 export default function App() {
   const [advice, setAdvice] = useState({ slip: { advice: "Click the dice to generete advices.", id: 1 } })
 
   return (
     <TailwindProvider>
-      <View className="flex items-center justify-center bg-[#202632] w-full py-20">
+      <SafeAreaView className="flex-1 items-center justify-center bg-[#202632] w-full py-20">
         <View className="relative items-center justify-evenly bg-[#313a49] min-h-[50%] w-11/12 max-w-md p-5 rounded-xl">
-          <Text className="text-[#53ffab] font-medium tracking-[0.35em] text-xs mb-6">ADVICE #{advice.slip.id}</Text>
-          <Text className="text-[#cee2e9] text-center font-semibold text-2xl mb-6 w-full">{advice.slip.advice}</Text>
+          <Text className="text-[#53ffab] font-medium text-xs mb-6 tracking-widest">ADVICE #{advice.slip.id}</Text>
+          <Animatable.Text animation="fadeIn" easing="ease-in-out" className="text-[#cee2e9] text-center font-semibold text-2xl mb-6 w-full">{advice.slip.advice}</Animatable.Text>
           <Image className="w-full h-4 mb-6" source={pattern} />
           <Button setAdvice={setAdvice} />
         </View>
         <StatusBar style="auto" />
-      </View>
+      </SafeAreaView>
     </TailwindProvider>
   )
 }
--- components/Button.js
+++ components/Button.js
@@ -1,20 +1,25 @@
-import { Image, Pressable, Text, View } from 'react-native'
+import { Image, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import icon from '../images/icon-dice.svg'
+import icon from '../images/icon-dice.png'
+import * as Animatable from 'react-native-animatable'
 
 export default function Button({ setAdvice }) {
 
-  function handlePress() {
-    fetch("	https://api.adviceslip.com/advice")
+  async function handlePress() {
+    const id = Math.floor(Math.random() * 200)
+
+    fetch(`https://api.adviceslip.com/advice/${id}`)
       .then(res => res.json())
       .then(data => setAdvice(data))
   }
 
   return (
-    <Pressable className="absolute -bottom-9 p-5 rounded-full bg-[#53ffab]" onPress={handlePress}>
-      <View className="">
-        <Image className="w-7 h-7" source={icon} />
-      </View>
-    </Pressable>
+    <Animatable.View animation="pulse" iterationCount="infinite" duration={2000} easing="ease-in-cubic" className="absolute -bottom-9">
+      <TouchableOpacity className="p-5 rounded-full bg-[#53ffab]" onPress={handlePress}>
+        <View>
+          <Image className="w-7 h-7" source={icon} />
+        </View>
+      </TouchableOpacity>
+    </Animatable.View>
   )
 }
